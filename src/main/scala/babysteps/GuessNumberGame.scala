7f101f8b0f67588323fa6bcc5c00b1e832f18331
GodPlaysChess <pumabox@gmail.com>
2015-05-31 15:33:48 +0200

    Bull-cows game

--- src/main/scala/babysteps/GuessNumberGame.scala
+++ src/main/scala/babysteps/GuessNumberGame.scala
@@ -3,55 +3,75 @@ package babysteps
 import com.nicta.rng.Rng
 
 import scala.language.higherKinds
-import scalaz.Alpha.M
-import scalaz.effect.{IORef, IO}
+import scalaz.MonadPlus
+import scalaz.effect.IO
 import scalaz.effect.IO._
-import scalaz.{Monad, MonadPlus}
 
 /**
  * 29-May-2015.
  */
 object GuessNumberGame {
   def main(args: Array[String]) {
-    GuessNumber.game.unsafePerformIO()
+    GuessNumber.bullsCows.unsafePerformIO()
   }
 }
 
 object GuessNumber {
 
-  def game: IO[Unit] = for {
+  def guessNumber: IO[Unit] = for {
     goal ← requireMaxNumber
     realNumber ← Rng.chooseint(0, goal.toInt).run
-    amountOfTries ← ioMonad.whileM(askGuess(realNumber), putStrLn("no"))
+    amountOfTries ← ioMonad.whileM(askGuess(realNumber), putStrLn("It is a wrong guess"))
     _ ← putStrLn(s"finally, you guessed from ${amountOfTries.size + 1} try")
   } yield ()
-  
-  def requireMaxNumber: IO[Int] =
-    ioMonad.iterateWhile(askAndReadNumber)(_ forall notDigit) map (_.toInt)
 
+  private def requireMaxNumber: IO[Int] =
+    ioMonad.iterateWhile(askAndReadNumber)(_ forall notDigit) map (_.toInt)
 
   private def notDigit: (Char) ⇒ Boolean =
     !Character.isDigit(_)
 
-  def askAndReadNumber: IO[String] =
+  private def askAndReadNumber: IO[String] =
     for {
-    _ ← putStrLn("Enter a number please")
-    maxN ← readLn
-  } yield maxN
+      _ ← putStrLn("Enter a number please")
+      maxN ← readLn
+    } yield maxN
 
-  def askGuess(number: Int): IO[Boolean] = for {
+  private def askGuess(number: Int): IO[Boolean] = for {
     _ ← putStrLn("Enter your guess")
     guess ← readLn
   } yield guess.toInt != number
 
   implicit val listMonadPlus: MonadPlus[List] = scalaz.std.AllInstances.listInstance
 
+  /* =============================================== */
 
-  def whileMprop[M[_], A](f: A ⇒ M[A])(a: A)(p: A ⇒ Boolean)(implicit M: Monad[M]): M[A] =
-    if (!p(a)) f(a) else M.bind(f(a))(n ⇒ whileMprop(f)(n)(p)(M))
+  def bullsCows: IO[Unit] = for {
+    given ← Rng.chooseint(1000, 10000).run
+    _ ← putStrLn(s"DEBUG: Secret number $given")
+    tuple ← ioMonad.iterateWhile(gameLoop(given, 0))(val_tries ⇒ winningCondition(given, val_tries._1))
+    tries = tuple._2
+    _ ← putStrLn(s"you won. $tries tries, not bad")
+  } yield ()
 
-  def whileCounting[M[_], A](f: A ⇒ M[A])(a: A)(p: A ⇒ Boolean)(implicit M: Monad[M]): M[A] =
-    if (!p(a)) f(a) else M.bind(f(a))(n ⇒ whileMprop(f)(n)(p)(M))
+  private def require4digitInt: IO[Int] = {
+    ioMonad.iterateWhile(askAndReadNumber)(s ⇒ (s forall notDigit) && (s.length == 4)) map (_.toInt)
+  }
 
+  private def gameLoop(given: Int, tries: Int): IO[(Int, Int)] = for {
+    guess ← require4digitInt
+    (bulls, cows) = calculateBullsCows(given, guess)
+    _ ← putStrLn(s"$bulls bulls, $cows cows")
+  } yield (guess, tries + 1)
+
+  def calculateBullsCows(given: Int, guess: Int): (Int, Int) = {
+    val bulls = given.toString.zip(guess.toString).count(t ⇒ t._1 == t._2)
+    val cows = given.toString.count(char ⇒ guess.toString.contains(char))
+    (bulls, cows - bulls)
+  }
+
+
+  private def winningCondition(goal: Int, entered: Int): Boolean =
+    goal != entered
 
 }
